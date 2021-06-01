457000ffa7ba42aaf05d15cb724767b2072a669b
Michael O'Connor <oconnor97@aol.com>
2021-06-01 11:52:52 -0400

    creating the indexedDB and creating an objectstore

--- public/db.js
+++ public/db.js
@@ -1,1 +1,17 @@
+let db;
+
+// Create a new request for a database
 const request = indexed.db.open('BudgetDB', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
+};
+
+request.onerror = function (event) {
+    console.log("There has been an error retrieving your data", event.target.errorCode);
+};
+
+
+
+
