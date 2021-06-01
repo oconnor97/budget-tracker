db13a9e4ca4495df24033af33f225884ae59f6b6
Michael O'Connor <oconnor97@aol.com>
2021-06-01 13:05:13 -0400

    changing the service worker to match a previous activity, debugging console errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,48 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/style.css",
-    "/db.js",
-    "/manifest.webmanifest",
+    '/',
+    '/index.html',
+    '/index.js',
+    '/db.js',
+    '/styles.css',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const STATIC_CACHE = 'static-cache';
+const DATA_CACHE = 'data-cache';
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
-            .open(PRECACHE)
+            .open(STATIC_CACHE)
             .then((cache) => cache.addAll(FILES_TO_CACHE))
-            .then(self.skipWaiting())
-    );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        return caches.delete(cacheToDelete);
-                    })
-                );
-            })
-            .then(() => self.clients.claim())
     );
 });
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
+    if (event.request.url.includes("/api/")) {
         event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return caches.open(RUNTIME).then((cache) => {
+            caches.open(DATA_CACHE).then(() => {
+                return caches.open(DATA_CACHE).then((cache) => {
                     return fetch(event.request).then((response) => {
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
+
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone())
+                        }
+                        return response;
                     });
                 });
             })
         );
+        return;
     }
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(response => {
+                if (response) return response;
+                if (event.request.headers.get('accept').includes('text/html')) return caches.match('/');
+            })
+        })
+    )
 });
